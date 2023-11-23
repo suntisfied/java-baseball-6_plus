cf2b3d349680f7f76161ce4d151c197e7ea33eb1
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-23 14:40:35 +0900

    reset everything for the second programming

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,17 +1,7 @@
 package baseball;
 
 public class Application {
-
     public static void main(String[] args) {
-        SequenceManager sequenceManager = new SequenceManager();
 
-        while (sequenceManager.validateRestart()) {
-
-            sequenceManager.proceedIntro();
-
-            sequenceManager.proceedMainGame();
-
-            sequenceManager.proceedOutro();
-        }
     }
 }
