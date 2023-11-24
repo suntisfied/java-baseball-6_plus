c8cd8fe2fc52168355fd6535990803c58ffbfcf4
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-24 22:17:52 +0900

    test(Comparator): ensure to calculate ball accurately

--- src/test/java/baseball/ComparatorTest.java
+++ src/test/java/baseball/ComparatorTest.java
@@ -5,11 +5,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 
 class ComparatorTest {
+    Comparator comparator = new Comparator(new UserAnswer("124"), new CorrectAnswer("123"));
+
+    @Test
+    public void calculateBall() {
+        int ball = comparator.calculateBall();
+
+        assertThat(ball).isEqualTo(0);
+    }
+
     @Test
     public void calculateStrike() {
-        var comparator = new Comparator(new UserAnswer("124"), new CorrectAnswer("123"));
-
         int strike = comparator.calculateStrike();
+
         assertThat(strike).isEqualTo(2);
     }
 }
