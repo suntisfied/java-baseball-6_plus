5aca94e4004149039f4dfb0472581c7ff80b5ec9
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-24 21:45:20 +0900

    refactor(Comparator): change internal logic to calculate strike
    Before: comparing each character
    Changed: converting to List and comparing each element
    This enhances expandability and maintainability of the code.

--- src/main/java/baseball/Comparator.java
+++ src/main/java/baseball/Comparator.java
@@ -1,17 +1,27 @@
 package baseball;
 
+import java.util.List;
+
 public class Comparator implements Comparable<UserAnswer> {
     @Override
     public int compareTo(UserAnswer userAnswer) {
         int matchedCount = 0;
 
-        var correctAnswer = new CorrectAnswer("123");
+        var userAnswerList = convertToList(userAnswer.answer());
+
+        var correctAnswerList = convertToList(new CorrectAnswer("123").answer());
 
         for (int i = 0; i < 3; i++) {
-            if (userAnswer.answer().charAt(i) == correctAnswer.answer().charAt(i)) {
+            if (userAnswerList.get(i).equals(correctAnswerList.get(i))) {
                 matchedCount++;
             }
         }
         return matchedCount;
+    }
+
+    public List<String> convertToList(String answer) {
+        return answer.chars()
+                .mapToObj(character -> String.valueOf((char) character))
+                .toList();
     }
 }
