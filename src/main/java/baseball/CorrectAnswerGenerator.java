83b1eb698a53570e0c53b63aa6827593f487f04f
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-25 11:41:39 +0900

    feat(baseball): generate correct answer in string

--- src/main/java/baseball/CorrectAnswerGenerator.java
+++ src/main/java/baseball/CorrectAnswerGenerator.java
@@ -1,6 +1,8 @@
 package baseball;
 
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class CorrectAnswerGenerator {
     private final Random random;
@@ -16,5 +18,13 @@ public class CorrectAnswerGenerator {
 
         int correctAnswerNumber = random.nextInt(9 * DigitLength) + DigitLength;
         return new CorrectAnswer(String.valueOf(correctAnswerNumber));
+    }
+
+    public CorrectAnswer generateStringCorrectAnswer() {
+        String correctAnswerString = IntStream.range(0, length)
+                .mapToObj(location -> (char) ('a' + random.nextInt(26)))
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+        return new CorrectAnswer(correctAnswerString);
     }
 }
