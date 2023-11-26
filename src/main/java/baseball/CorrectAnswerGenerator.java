584b4cec423bf61fbb309f96dd4e0e3479230eed
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-26 11:24:46 +0900

    fix(baseball): ensure to generate unique random alphabets

--- src/main/java/baseball/CorrectAnswerGenerator.java
+++ src/main/java/baseball/CorrectAnswerGenerator.java
@@ -3,19 +3,16 @@ package baseball;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class CorrectAnswerGenerator {
     private final int gameSize;
     private final int gameType;
-    private final Random random;
 
     public CorrectAnswerGenerator(int gameSize, int gameType) {
         this.gameSize = gameSize;
         this.gameType = gameType;
-        random = new Random();
     }
 
     public final CorrectAnswer generateCorrectAnswer() {
@@ -37,10 +34,18 @@ public class CorrectAnswerGenerator {
     }
 
     private CorrectAnswer generateStringCorrectAnswer() {
-        String correctAnswerString = IntStream.range(0, gameSize)
-                .mapToObj(location -> (char) ('a' + random.nextInt(26)))
-                .map(String::valueOf)
-                .collect(Collectors.joining());
+        List<Character> correctAnswerAlphabets =
+                new ArrayList<>(IntStream.rangeClosed('a', 'z')
+                        .mapToObj(numberValue -> (char) numberValue)
+                        .toList());
+        Collections.shuffle(correctAnswerAlphabets);
+
+        String correctAnswerString =
+                correctAnswerAlphabets.stream()
+                        .limit(gameSize)
+                        .map(String::valueOf)
+                        .collect(Collectors.joining());
+
         return new CorrectAnswer(correctAnswerString);
     }
 }
